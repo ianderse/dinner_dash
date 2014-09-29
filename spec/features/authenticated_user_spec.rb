228105215f2498fd0b8a8d01f11f05c0d93ab4e7
Ian Andersen <ianderse@mac.com>
2014-09-29 11:21:25 -0600

    add travis.yml file

---
+++ spec/features/authenticated_user_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+#
+#
+#
+#
+#
+
+# describe 'authenticated user', type: :feature do
+#   let(:user) { User.create()}
+
+# end
