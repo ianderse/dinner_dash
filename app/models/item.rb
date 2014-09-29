e5520eedb18e0736b402f53e391e200da5311123
Luke Aiken <lukeaiken@gmail.com>
2014-09-29 14:57:25 -0600

    order and line_item models and migrations and relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
+  has_many :line_items
+  has_many :orders, through: :line_items
 	validates :title, :description, presence: true
 	validates :price, numericality: { greater_than: 0 }
 end
