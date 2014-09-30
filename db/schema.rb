b8968f36cb5f73b7a3be52d3336980db9c8bc981
Jessica Goulding <jgoulding2@gmail.com>
2014-09-30 12:20:39 -0600

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -17,15 +17,11 @@ ActiveRecord::Schema.define(version: 20140930013811) do
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
-    t.string   "title"
-    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "item_categories", force: true do |t|
-    t.integer  "item_id"
-    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
