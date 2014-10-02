3f83729cd10641db9a7d1f2610fd76f27d9d5b2a
Jessica Goulding <jgoulding2@gmail.com>
2014-10-02 13:47:05 -0600

    changed production env for fog, missing c in access
    changed development to be using s3 image upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,14 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :fog,
     :fog_credentials => {
-      provider: "AWS"
-      aws_access_key_id: "AWS_ACCESS_KEY_ID"
-      aws_secret_acess_key: "AWS_SECRET_ACCESS_KEY"
+      :provider =>"AWS",
+      :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
     },
-    :fog_directory => "BUCKET_NAME"
+    :fog_directory => ENV["BUCKET_NAME"]
   }
-  
-  #
+
+
   #   :bucket => ENV['S3_BUCKET_NAME'],
   #   :s3_credentials => {
   #     :s3_host_name => 'turingproject.s3.amazonaws.com',
