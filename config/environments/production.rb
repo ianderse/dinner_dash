b4835b560a2cb0615237af43b9acb64e967f3800
Jessica Goulding <jgoulding2@gmail.com>
2014-10-02 14:08:30 -0600

    removed extra access keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,7 @@ Rails.application.configure do
     :fog_credentials => {
       :provider => "AWS",
       :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
     :fog_directory => ENV["BUCKET_NAME"]
   }
