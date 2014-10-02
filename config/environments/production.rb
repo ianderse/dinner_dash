2c2a5cb9cf111a18444f53f55b461a48c017469f
Ian Andersen <ianderse@mac.com>
2014-10-02 12:19:27 -0600

    fix env settings for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :fog,
     :fog_credentials => {
-      provider: "AWS"
-      aws_access_key_id: "AWS_ACCESS_KEY_ID"
-      aws_secret_acess_key: "AWS_SECRET_ACCESS_KEY"
+      :provider => "AWS"
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID']
+      :aws_secret_acess_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :fog_directory => "BUCKET_NAME"
+    :fog_directory => ENV['S3_BUCKET_NAME']
   }
-  
+
   #
   #   :bucket => ENV['S3_BUCKET_NAME'],
   #   :s3_credentials => {
