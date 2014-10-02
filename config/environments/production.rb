2429115dbeb5fd43614dfb74579ec6d0e903ef88
Jessica Goulding <jgoulding2@gmail.com>
2014-10-02 10:04:08 -0600

    styling WIP
    working on pulling form out of navbar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :s3_hostname => ENV['S3_HOST_NAME']
     }
   }
 end
