a8a28890298b216906c10641e798c371a35484e8
Luke Aiken <lukeaiken@gmail.com>
2014-10-02 23:21:13 -0600

    spec for users show

--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -3,8 +3,9 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'admin user', type: :feature do
+  let(:user) { create(:user, role:'admin') }
+
 	before do
-		user = create(:user, first_name: 'joe', email: 'abc@example.com', password: 'asdf', password_confirmation: 'asdf', role:'admin')
     visit '/'
     fill_in 'email', with: "#{user.email}"
     fill_in 'password', with: "#{user.password}"
@@ -15,4 +16,14 @@ describe 'admin user', type: :feature do
 		visit '/'
 		expect(page).to have_content("administrator")
 	end
+
+  it "can see all users" do
+    visit '/users'
+    expect(page).to have_content("Users")
+  end
+
+  it "can see the profile of an individual user" do
+    visit "/users/#{user.id}"
+    expect(page).to have_content("User Page")
+  end
 end
