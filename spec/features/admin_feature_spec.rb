b869af527d67bcae86421b59aabd3cb4cdb8c872
Jessica Goulding <jgoulding2@gmail.com>
2014-10-03 07:13:07 -0600

    more seeds and some testing

--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -11,8 +11,45 @@ describe 'admin user', type: :feature do
     click_on 'login'
 	end
 
+
 	it 'has a role of admin' do
 		visit '/'
 		expect(page).to have_content("administrator")
 	end
+
+	# As an authenticated Administrator, I can:
+# Create item listings including a name, description, price, and upload a photo
+# describe 'admin user', type: :feature	do
+#
+	it 'can create item listings' do
+		visit '/items/4'
+		expect(page).to have_content("add new item")
+	end
+
+	it 'can create item listings with title, description, price' do
+		visit 'items/4/edit'
+		expect(page).to have_content("title", "description", "price")
+	end
+
+	it 'can create item listings with category' do
+		visit 'items/4/edit'
+		expect(page).to have_css '#categories_'
+	end
+
+	it 'can create item listsing with photo' do
+		visit 'items/4/edit'
+		expect(page).to have_css '#image'
+	end
+
+
+#
+
+# Modify existing items’ name, description, price, and photo
+# Create named categories for items (eg: "Small Plates")
+# Assign items to categories or remove them from categories. Products can belong to more than one category.
+# Retire a item from being sold, which hides it from browsing by any non-administrator
+
+
+
+
 end
