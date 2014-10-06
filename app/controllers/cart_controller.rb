82bab6174e7732245f62fbf9768a2f593c50d6b6
Luke Aiken <lukeaiken@gmail.com>
2014-10-05 20:47:06 -0600

    serving js

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,15 +4,21 @@ class CartController < ApplicationController
   end
 
   def add_item
+    @item_id = params[:item_id]
     session[:cart] ||= []
-    session[:cart] << params[:item_id]
+    session[:cart] << @item_id
     flash[:notice] = "Item added to your cart!"
+
     redirect_to items_path
+    #respond_to do |format|
+    #  format.js { @item_id } 
+    #end
   end
 
   def remove_item
     @item_id = params[:item_id]
     session[:cart].delete(@item_id)
+
     respond_to do |format|
       format.js { @item_id }
     end
