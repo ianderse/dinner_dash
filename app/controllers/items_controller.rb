c9dd607b7572e0f1a47009c0825202293c010466
Ian Andersen <ianderse@mac.com>
2014-10-07 09:09:30 -0600

    add active scope to Item class

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 	load_and_authorize_resource
 
 	def index
-		@items = Item.where(:active => 'true')
+		@items = Item.active
 		@categories = Category.all
 	end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,6 @@ class Item < ActiveRecord::Base
 
 	has_attached_file :image, styles: {:medium => "300x300>", :thumb => "100x100"}
 	validates_attachment :image, content_type: {content_type: ["image/jpeg", "image/jpeg", "image/png", "image/gif"]}
+
+	scope :active, -> {where(active: true)}
 end
