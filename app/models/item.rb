4de8299b2cb9395cbb87d0670623b17e6ae39fbc
Jessica Goulding <jgoulding2@gmail.com>
2014-10-07 11:59:56 -0600

    added styling to grid
    changed pictures to all be 300x300

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ActiveRecord::Base
 	has_many :item_categories
 	has_many :categories, through: :item_categories
 
-	has_attached_file :image, styles: {:medium => "300x300>", :thumb => "100x100"}
+	has_attached_file :image, styles: {:medium => "300x300#", :thumb => "100x100"}
 	validates_attachment :image, content_type: {content_type: ["image/jpeg", "image/jpeg", "image/png", "image/gif"]}
 
 	scope :active, -> {where(active: true)}
