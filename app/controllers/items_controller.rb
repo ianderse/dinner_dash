66bfd38c1f5d3070e72f44961d0e1f6ee1df2950
Ian Andersen <ianderse@mac.com>
2014-10-13 10:54:03 -0600

    ability to step 1/2 star in number field, displays average of reviews on item page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,12 @@ class ItemsController < ApplicationController
 	def show
 		@item = Item.find(params[:id])
 		@reviews = Review.where(item_id: @item.id)
+		#can move average to item model?
+		@average = @reviews.map do |review|
+									review.stars
+								end
+		@average = @average.reduce(:+) / @reviews.size
+
 		@categories = @item.categories
 	end
 end
