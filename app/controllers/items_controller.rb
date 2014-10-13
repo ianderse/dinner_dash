803af6bd4af6e4a1abfa6ca55b2990653263a862
Ian Andersen <ianderse@mac.com>
2014-10-13 15:41:42 -0600

    round average to nearest 0.5, remove luke's pdf stuff in the item index for now

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,22 +4,17 @@ class ItemsController < ApplicationController
 	def index
 		@items = Item.active
 		@categories = Category.all
-
-    respond_to do |format|
-      format.html
-      format.pdf { render pdf: "menu" }
-    end
 	end
 
 	def show
 		@item = Item.find(params[:id])
 		@reviews = Review.where(item_id: @item.id)
-		#can move average to item model?
+		#move average to item model?
 		if !@reviews.empty?
 			@average = @reviews.map do |review|
 										review.stars
 									end
-			@average = @average.reduce(:+) / @reviews.size
+			@average = ((@average.reduce(:+) / @reviews.size) * 2.0).round / 2.0
 		else
 			@average = 0
 		end
