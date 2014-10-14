d1906ec5d2f99b4261a016ccc0473e128822db37
Luke Aiken <lukeaiken@gmail.com>
2014-10-14 11:58:34 -0600

    refactor admin items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < Admin::BaseController
-  load_and_authorize_resource
+
+  before_action :set_item, only: [:update, :destroy, :show, :edit]
 
   def new
     @item = Item.new
@@ -7,17 +8,14 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def show
-    @item = Item.find(params[:id])
     @categories = @item.categories
   end
 
   def edit
-    @admin_item = Item.find(params[:id])
     @categories = Category.all
   end
 
   def create
-    #refactor this
     @item = Item.new(item_params)
     @categories = params[:categories] || []
     @categories.each do |category|
@@ -34,24 +32,21 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to admin_path
   end
 
   def update
-    #refactor this
-    @admin_item = Item.find(params[:id])
     @categories = params[:categories] || []
-    @admin_item.categories = []
+    @item.categories = []
     @categories.each do |category|
       category = Category.find(category)
-      @admin_item.categories << category
+      @item.categories << category
     end
 
-    if @admin_item.update(item_params)
+    if @item.update(item_params)
       flash[:notice] = "Your item has been successfully updated!"
-      redirect_to admin_item_path(@admin_item)
+      redirect_to admin_item_path(@item)
     else
       redirect_to :back
       flash[:notice] = "Error saving item."
@@ -62,6 +57,10 @@ class Admin::ItemsController < Admin::BaseController
 
     def item_params
       params.require(:item).permit(:title, :description, :price, :image, :category, :active)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
     end
 
 end
