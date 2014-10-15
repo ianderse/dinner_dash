be2a611f94549004f716ea8dac594112e0727a3e
Luke Aiken <lukeaiken@gmail.com>
2014-10-15 16:54:08 -0600

    spec for pdf actions

--- spec/controllers/pdf_delivery_spec.rb
+++ spec/controllers/pdf_delivery_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  it "sends a reqeust for a pdf file" do
+  it "sends a reqeust for a pdf file inline" do
     create(:item)
 
     get :menu, format: :pdf, disposition: "inline"
@@ -9,6 +9,17 @@ RSpec.describe ItemsController, type: :controller do
     expect(response.body).to include("PDF")
     expect(response.headers["Content-Transfer-Encoding"]).to eq("binary")
     expect(response.headers["Content-Disposition"]).to eq("inline; filename=\"caussa.pdf\"")
+    expect(response.headers["Content-Type"]).to eq("application/pdf")
+  end
+
+  it "sends a reqeust for a pdf file attachment" do
+    create(:item)
+
+    get :menu, format: :pdf, disposition: "attachment"
+    
+    expect(response.body).to include("PDF")
+    expect(response.headers["Content-Transfer-Encoding"]).to eq("binary")
+    expect(response.headers["Content-Disposition"]).to eq("attachment; filename=\"caussa.pdf\"")
     expect(response.headers["Content-Type"]).to eq("application/pdf")
   end
 end
